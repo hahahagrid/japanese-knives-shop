101326d60976bc39cd17de198bb1bb086972e8dd
hahahagrid <kidkimsky@gmail.com>
2026-03-30 09:21:00 +0200

    feat: add knives to products migration and update start script to run migrations automatically

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20260328_142405_init from './20260328_142405_init';
 import * as migration_20260329_090000_add_youtube_url from './20260329_090000_add_youtube_url';
 import * as migration_20260329_112419_add_order_number from './20260329_112419_add_order_number';
+import * as migration_20260330_071532_knives_to_products from './20260330_071532_knives_to_products';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20260329_112419_add_order_number.up,
     down: migration_20260329_112419_add_order_number.down,
-    name: '20260329_112419_add_order_number'
+    name: '20260329_112419_add_order_number',
+  },
+  {
+    up: migration_20260330_071532_knives_to_products.up,
+    down: migration_20260330_071532_knives_to_products.down,
+    name: '20260330_071532_knives_to_products'
   },
 ];
